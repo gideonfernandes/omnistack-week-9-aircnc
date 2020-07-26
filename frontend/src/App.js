29ac3944c3b10935277124d6ab3fd84454f551a6
Gideon Fernandes <gideon.de.fernandes@gmail.com>
2020-07-26 11:27:32 -0300

    Add initial CSS and first screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import './App.css';
+import logo from './assets/logo.svg';
 
 function App() {
   return (
-    <div className="App">Hello World!</div>
+    <div className="container">
+      <img src={logo} alt="Logo AirCnC"/>
+      <main className="content">
+        <h1>
+          Ofereça <strong>spots</strong> para programadores
+          e encontre <strong>talentos</strong> para sua empresa
+        </h1>
+
+        <form>
+          <label htmlFor="email">EMAIL *</label>
+          <input type="email" name="email" placeholder="Seu melhor e-mail" />
+          <button type="submit" className="btn">Cadastrar</button>
+        </form>
+      </main>
+    </div>
   );
 };
 
